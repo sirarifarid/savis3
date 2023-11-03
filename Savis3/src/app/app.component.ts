9e61472bb5a979a6103af53507e27ca734514e96
Jason Yu <jasonyu@csus.edu>
2023-11-02 18:19:51 -0700

    angular 12 tech stack

--- Savis3/src/app/app.component.ts
+++ Savis3/src/app/app.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from './data.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  template: `
-  <div>
-    {{ message }}
-  <div>
-  `
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Savis3';
-  message = '';
-
-  constructor(private dataService: DataService) {}
-
-  ngOnInit() {
-      this.dataService.getTestData().subscribe(data => {
-        this.message = data.message
-      })
-  }
 }
