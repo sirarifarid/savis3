4619d3fed1b65fba81ee0c472a89d0c8ab5e65d3
Jason Yu <jasonyu@csus.edu>
2023-11-12 16:19:25 -0800

    Utils, setup for one-proportion component & routing for login/signup

--- Savis3/src/app/app.module.ts
+++ Savis3/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OneProportionComponent } from './one-proportion/one-proportion.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneProportionComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, { enableTracing: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
