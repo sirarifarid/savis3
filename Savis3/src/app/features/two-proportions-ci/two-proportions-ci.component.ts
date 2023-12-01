e25bdf90c9f030343514d0e84040d28346b7445a
horhal <adibkhan1987@gmail.com>
2023-11-30 21:30:22 -0800

    migrated most of the .ts, html, and scss files

--- Savis3/src/app/features/two-proportions-ci/two-proportions-ci.component.ts
+++ Savis3/src/app/features/two-proportions-ci/two-proportions-ci.component.ts
@@ -1,9 +1,11 @@
+/*
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-two-proportions-ci',
   templateUrl: './two-proportions-ci.component.html',
-  styleUrls: ['./two-proportions-ci.component.scss']
+  styleUrls: ['./two-proportions-ci.component.scss', './../scss/base.scss']
 })
 export class TwoProportionsCIComponent implements OnInit {
 
@@ -12,4 +14,61 @@ export class TwoProportionsCIComponent implements OnInit {
   ngOnInit(): void {
   }
 
+}
+*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-proportions-ci',
+  templateUrl: './two-proportions-ci.component.html',
+  styleUrls: ['./two-proportions-ci.component.scss', './../scss/base.scss']
+})
+export class TwoProportionsCIComponent implements OnInit {
+  numASuccess: number = 0;
+  numAFailure: number = 0;
+  numBSuccess: number = 0;
+  numBFailure: number = 0;
+  numSimulations: number = 1;
+  confidenceLevel: number = 95;
+
+  // Add these properties if they are used for displaying data
+  sampleProportionA: number | null = null;
+  sampleProportionB: number | null = null;
+  sampleProportionDiff: number | null = null;
+  sampleAFailure: number | null = null;
+  sampleASuccess: number | null = null;
+  sampleBFailure: number | null = null;
+  sampleBSuccess: number | null = null;
+  isDataLoaded: boolean = false; // Example property for disabling buttons
+
+  // Add these new properties for the template
+  proportionDiff: number | null = null;
+  meanSampleDiffs: number | null = null;
+  stddevSampleDiffs: number | null = null;
+  lowerBound: number | null = null;
+  upperBound: number | null = null;
+  totalSamples: number | null = null;
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initialization logic here
+  }
+
+  // Make sure to implement these methods
+  loadData(): void {
+    // Your logic here
+  }
+
+  runSimulations(): void {
+    // Your logic here
+  }
+
+  buildConfidenceInterval(): void {
+    // Your logic here
+  }
+
+  // Add other methods and logic as needed
 }
